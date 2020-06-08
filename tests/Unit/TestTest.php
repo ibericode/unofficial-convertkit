70a29d72a8634268bea73792e2ae9c213eedefba
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-08 12:34:05 +0200

    Add CI

---
+++ tests/Unit/TestTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UnofficialConvertKit\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function firstTest()
+    {
+        $this->assertEquals(true, true);
+    }
+}
