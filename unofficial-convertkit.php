3eee99383a621ecf086cdee3a1a4b8dd922c421f
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-09 09:56:41 +0200

    Add headers for wordpress

--- unofficial-convertkit.php
+++ unofficial-convertkit.php
@@ -1,3 +1,16 @@
 <?php
-//Create error to tests sniff
-echo 'hello world';
+/**
+ * Plugin Name:       Unofficial ConvertKit
+ * Plugin URI:
+ * Description:       Wordpress plugin for ConvertKit
+ * Version:           0.0.0
+ * Requires at least: 7
+ * Requires PHP:      7.2
+ * License:           GPL-3.0-only
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       unofficial-convertkit
+ * Domain Path:       /languages
+ */
+
+namespace UnofficialConvertKit;
+
