154f9ec289d64bbac325296228bceac66675eb61
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-10 11:25:01 +0200

    Add php unit and wordpress unit utils

---
+++ tests/TestTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UnofficialConvertKit\Tests;
+
+use WP_UnitTestCase_Base;
+
+class TestTest extends WP_UnitTestCase_Base
+{
+
+    /**
+     * @test
+     */
+    public function firstTest()
+    {
+        $this->assertEquals(true, true);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php 
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+define('WP_TESTS_CONFIG_FILE_PATH', __DIR__ . '/wp-tests-config.php');
+
+require __DIR__ . '/../vendor/wordpress/phpunit/bootstrap.php';
