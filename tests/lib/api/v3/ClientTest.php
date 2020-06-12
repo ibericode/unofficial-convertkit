825a51e591329130b09d3b757fa951deaae417a1
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-12 14:15:35 +0200

    Add data provider for test

--- tests/lib/api/v3/ClientTest.php
+++ tests/lib/api/v3/ClientTest.php
@@ -2,6 +2,7 @@
 
 namespace UnofficialConvertKit\Tests\lib\api\v3;
 
+use InvalidArgumentException;
 use UnofficialConvertKit\API\V3\Client;
 use UnofficialConvertKit\API\V3\Not_Found_Exception;
 use UnofficialConvertKit\API\V3\Unauthorized_Exception;
@@ -13,54 +14,47 @@ class ClientTest extends TestCase {
 	/**
 	 * @test
 	 */
-	public function throws_unauthorised_exception_when_API_key_is_invalid() {
-		$this->expectException( Unauthorized_Exception::class );
+	public function api_key_and_secret_can_not_be_empty() {
+		$this->expectException( InvalidArgumentException::class );
 
-		$api_key = 'invalid_key';
+		new Client( '', '' );
+	}
+
+	/**
+	 * @test
+	 *
+	 * @dataProvider responseProvider
+	 */
+	public function throws_exception_when_certain_http_status_codes_are_returned( string $exception, string $code, string $message, string $response_file ) {
+		$this->expectException( $exception );
 
 		WP_Mock::userFunction( 'add_query_arg' )
-			   ->andReturn( Client::API_BASE_URL );
+		       ->andReturn( Client::API_BASE_URL );
 
 		WP_Mock::userFunction( 'wp_remote_request' )
-				->andReturn( array() );
+		       ->andReturn( array() );
 
 		WP_Mock::userFunction( 'wp_remote_retrieve_response_code' )
-				->andReturn( '401' );
+		       ->andReturn( $code );
 
 		WP_Mock::userFunction( 'wp_remote_retrieve_response_message' )
-				->andReturn( 'Unauthorised' );
+		       ->andReturn( $message );
 
 		WP_Mock::userFunction( 'wp_remote_retrieve_body' )
-				->andReturn( file_get_contents( __DIR__ . '/mock/unauthorised_response.json' ) );
+		       ->andReturn( file_get_contents( __DIR__ . '/mock/'. $response_file ) );
 
-		$client = new Client( $api_key, '' );
+		$client = new Client( 'api_key', '' );
 
 		$client->get( 'account' );
 	}
 
 	/**
-	 * @test
+	 * @return array[]
 	 */
-	public function throws_not_found_exception_on_non_existing_resource() {
-		$this->expectException( Not_Found_Exception::class );
-
-		WP_Mock::userFunction( 'add_query_arg' )
-			   ->andReturn( Client::API_BASE_URL );
-
-		WP_Mock::userFunction( 'wp_remote_request' )
-			   ->andReturn( array() );
-
-		WP_Mock::userFunction( 'wp_remote_retrieve_response_code' )
-			   ->andReturn( '404' );
-
-		WP_Mock::userFunction( 'wp_remote_retrieve_response_message' )
-			   ->andReturn( 'Not found' );
-
-		WP_Mock::userFunction( 'wp_remote_retrieve_body' )
-			   ->andReturn( file_get_contents( __DIR__ . '/mock/404.html' ) );
-
-		$client = new Client( 'api_key', '' );
-
-		$client->get( 'account' );
+	public function responseProvider() {
+		return array(
+			array( Unauthorized_Exception::class, '401', 'Unauthorised', 'unauthorised_response.json'),
+			array( Not_Found_Exception::class, '404', 'Not Found', '404.html')
+		);
 	}
 }
