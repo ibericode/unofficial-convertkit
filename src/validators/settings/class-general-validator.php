1be3e421aaff4c1f773bc5f1bfc417d0f75dae10
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-12 12:47:48 +0200

    Validate the API secret and key against the api

--- src/validators/settings/class-general-validator.php
+++ src/validators/settings/class-general-validator.php
@@ -2,6 +2,9 @@
 
 namespace UnofficialConvertKit;
 
+use function UnofficialConvertKit\API\V3\is_valid_api_key;
+use function UnofficialConvertKit\API\V3\is_valid_api_secret;
+
 class General_Validator implements Validator_Interface {
 
 	/**
@@ -12,20 +15,43 @@ class General_Validator implements Validator_Interface {
 	 * @return Validation_Error[]
 	 */
 	public function validate( $data ): array {
+		$api_key    = $data['api_key'] ?? null;
+		$api_secret = $data['api_secret'] ?? null;
+
 		$errors = array();
 
-		if ( empty( $data['api_key'] ) ) {
+		if ( empty( $api_key ) ) {
 			$errors[] = new Validation_Error(
 				__( 'Your API key can not be empty.', 'unofficial-convertkit' ),
 				'api-key-empty'
 			);
 		}
 
-		if ( empty( $data['api_secret'] ) ) {
+		if ( empty( $api_secret ) ) {
 			$errors[] = new Validation_Error(
 				__( 'Your API secret can not be empty.', 'unofficial-convertkit' ),
 				'api-secret-empty'
 			);
+		}
+
+		//Don't fire a lot of errors.
+		if ( count( $errors ) > 0 ) {
+			return $errors;
+		}
+
+		if ( ! is_valid_api_key( $api_key ) ) {
+			$errors[] = new Validation_Error(
+				__( 'Your API key is invalid.', 'unofficial-convertkit' ),
+				'api-key-invalid'
+			);
+		}
+
+		if ( ! is_valid_api_secret( $api_secret ) ) {
+			$errors[] = new Validation_Error(
+				__( 'Your API secret is invalid.', 'unofficial-convertkit' ),
+				'api-secret-invalid'
+			);
+
 		}
 
 		return $errors;
