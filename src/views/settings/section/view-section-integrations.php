7fb4f66cfb686a8f26779c78eb8171885d9215af
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-16 11:05:41 +0200

    Renamed files

---
+++ src/views/settings/section/view-section-integrations.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace UnofficialConvertKit;
+
+/**
+ * @internal
+ */
+return function() {
+	//ToDo: implement view
+	?>
+		<h3>Integration's</h3>
+		<table class="widefat striped">
+			<thead>
+				<tr>
+					<td><?php _e( 'Name', 'unofficial-convertkit' ); ?></td>
+					<td><?php _e( 'Description', 'unofficial-convertkit' ); ?></td>
+					<td><?php _e( 'Status', 'unofficial-convertkit' ); ?></td>
+				</tr>
+			</thead>
+            <tbody>
+
+            </tbody>
+		</table>
+	<?php
+};
