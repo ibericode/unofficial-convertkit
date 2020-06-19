717043c00ef55f3a831959a1dc8cb016440b74c9
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-19 13:46:49 +0200

    Add `lists_forms` method to REST API class

--- src/lib/api/v3/class-rest-api.php
+++ src/lib/api/v3/class-rest-api.php
@@ -2,6 +2,8 @@
 
 namespace UnofficialConvertKit\API\V3;
 
+use stdClass;
+
 /**
  * Contains methods with defined resource as end point
  *
@@ -24,5 +26,16 @@ class REST_API {
 	 */
 	public function __construct( string $api_key, string $api_secret ) {
 		$this->client = new Client( $api_key, $api_secret );
+	}
+
+	/**
+	 * @return stdClass
+	 *
+	 * @see https://developers.convertkit.com/#list-forms
+	 *
+	 * @throws Response_Exception
+	 */
+	public function lists_forms(): stdClass {
+		return $this->client->post( 'forms' );
 	}
 }
