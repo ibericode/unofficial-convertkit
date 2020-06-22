d118a6e594b784af1cedcb5c5b4dd906fae6c8ef
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-22 09:34:19 +0200

    Add comment

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -10,4 +10,5 @@ require UNOFFICIAL_CONVERTKIT_SRC_DIR . '/core/hooks/interface-hooks.php';
 require UNOFFICIAL_CONVERTKIT_SRC_DIR . '/core/hooks/interface-hooker.php';
 require UNOFFICIAL_CONVERTKIT_SRC_DIR . '/core/hooks/class-hooks-service.php';
 
+//Require integrations to be load
 require UNOFFICIAL_CONVERTKIT_SRC_DIR . '/integrations/bootstrap.php';
