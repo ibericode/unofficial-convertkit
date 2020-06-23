1a1fd772c51baae3cd733987d2a18378649a40a5
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-23 15:51:02 +0200

    load the registration form integration

--- src/integrations/hooks/class-integrations-hooks.php
+++ src/integrations/hooks/class-integrations-hooks.php
@@ -15,6 +15,7 @@ class Integrations_Hooks implements Hooks {
 		//Todo: only add the hooks when needed.
 		$integrations = new Integration_Repository( $hooker );
 		$integrations->add_integration( new Comment_Form_Integration() );
+		$integrations->add_integration( new Registration_Form_Integration() );
 
 		if ( is_admin() ) {
 			require __DIR__ . '/../admin/hooks/class-integrations-hooks.php';
