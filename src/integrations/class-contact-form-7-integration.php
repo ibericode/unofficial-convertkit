cc1d62bdbbef99f33c4d716e17f6945af429a426
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-23 15:50:03 +0200

    Add integration classes for contact form 7

---
+++ src/integrations/class-contact-form-7-integration.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace UnofficialConvertKit\Integration;
+
+use UnofficialConvertKit\Hooks;
+
+class Contact_Form_7_Integration implements Integration {
+
+	const IDENTIFIER = 'contact-form-7';
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_identifier(): string {
+		return self::IDENTIFIER;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_name(): string {
+		return 'Contact Form 7';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_description(): string {
+		return __( 'Subscribes people from Contact Form 7 forms.', 'unofficial_convertkit' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function is_available(): bool {
+		return is_plugin_active( 'contact-form-7' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function is_active(): bool {
+		//TODO: manage with the settings form
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_options(): array {
+		// TODO: manage with the settings form
+
+		return array(
+			'form-ids' => array( 1441335 ),
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function actions(): array {
+		return array();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_hooks(): Hooks {
+		require __DIR__ . '/hooks/class-contact-form-7-hooks.php';
+
+		return new Contact_Form_7_Hooks();
+	}
+}
---
+++ src/integrations/hooks/class-contact-form-7-hooks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UnofficialConvertKit\Integration;
+
+use UnofficialConvertKit\Hooker;
+use UnofficialConvertKit\Hooks;
+
+class Contact_Form_7_Hooks implements Hooks {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function hook( Hooker $hooker ) {
+		// TODO: Implement hook() method.
+	}
+}
