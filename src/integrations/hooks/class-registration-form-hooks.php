e28ee3cf4673a6f5b45f85c1f9c099fee996b3f4
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-23 15:24:11 +0200

    Add hooks class for registration form

--- src/integrations/hooks/class-registration-form-hooks.php
+++ src/integrations/hooks/class-registration-form-hooks.php
@@ -1,1 +1,14 @@
 <?php
+
+use UnofficialConvertKit\Hooker;
+use UnofficialConvertKit\Hooks;
+
+class Registration_Form_Hooks implements Hooks {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function hook( Hooker $hooker ) {
+		// TODO: Implement hook() method.
+	}
+}
