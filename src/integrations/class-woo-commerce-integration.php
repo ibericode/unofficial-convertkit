9744c1ee64ac32c7b3416cf5750bea6312d901a7
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-24 09:16:16 +0200

    Add classes woo commerce integration

---
+++ src/integrations/class-woo-commerce-integration.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace UnofficialConvertKit\Integration;
+
+use UnofficialConvertKit\Hooks;
+
+class Woo_Commerce_Integration implements Integration {
+
+	const IDENTIFIER = 'woo_commerce';
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_identifier(): string {
+		return self::IDENTIFIER;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_name(): string {
+		return 'Woo Commerce Checkout';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_description(): string {
+		return __( 'Subscribes people from WooCommerce\'s checkout form.', 'unofficial-converkit' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function is_available(): bool {
+		// TODO: Implement is_available() method.
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function is_active(): bool {
+		return true;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_options(): array {
+		return array(
+			'forms-id' => array( 1441335 ),
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function actions(): array {
+		//TODO: find the hooks of woo commerce
+		return array();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_hooks(): Hooks {
+		require __DIR__ . '/hooks/class-woo-commerce-hooks.php';
+
+		return new Woo_Commerce_Hooks();
+	}
+}
---
+++ src/integrations/hooks/class-woo-commerce-hooks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace UnofficialConvertKit\Integration;
+
+use UnofficialConvertKit\Hooker;
+use UnofficialConvertKit\Hooks;
+
+class Woo_Commerce_Hooks implements Hooks {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function hook( Hooker $hooker ) {
+		// TODO: Implement hook() method.
+	}
+}
