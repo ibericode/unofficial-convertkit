69b1d29c7d7366505a5176d8215c8039c4de66cf
Luuk van Kooten <luukvankooten@gmail.com>
2020-06-30 14:45:56 +0200

    Add class

--- src/views/integrations/admin/forms/view-comment-form-integration.php
+++ src/views/integrations/admin/forms/view-comment-form-integration.php
@@ -4,8 +4,7 @@ use UnofficialConvertKit\Integrations\Comment_Form_Integration;
 
 return static function( Comment_Form_Integration $integration, stdClass $forms ) {
 	?>
-
-	<table>
+	<table class="form-table">
 		<tbody>
 			<?php call_user_func( require __DIR__ . '/../components/table/default-rows.php', $integration, $forms ); ?>
 		</tbody>
