81d030d7c2f9228485806b02389d407f20598ee9
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-02 11:06:34 +0200

    Add name to checkbox label

--- src/views/integrations/comment-form-select-box.php
+++ src/views/integrations/comment-form-select-box.php
@@ -7,7 +7,11 @@
 return static function( string $checkbox_label ) {
 	?>
 		<label>
-			<input type="checkbox" value="1">
+			<input
+				type="checkbox"
+				value="1"
+				name="unofficial_convertkit_integrations_subscribe"
+			>
 			<?php echo $checkbox_label; ?>
 		</label>
 	<?php
