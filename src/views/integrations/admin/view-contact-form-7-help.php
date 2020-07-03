0c719cd71e883d168dbe6bd0010b19575a1d57cb
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-03 10:15:52 +0200

    Changed form tag name

--- src/views/integrations/admin/view-contact-form-7-help.php
+++ src/views/integrations/admin/view-contact-form-7-help.php
@@ -10,7 +10,7 @@ return static function() {
 		printf(
 			/* translators: %s: input form markup  */
 			esc_html__( 'To integrate with Contact Form 7, configure the settings below and then add %s to your CF7 form mark-up.', 'unofficial-converkit' ),
-			'<input type="text" onfocus="this.select()" readonly="" value="[mc4wp_checkbox]">'
+			'<input type="text" size="29" onfocus="this.select()" readonly="" value="[unofficial_convertkit_checkbox]">'
 		);
 	?>
 		</p>
