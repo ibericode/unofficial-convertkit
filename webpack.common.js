0c6528987fb1b08eaaa35383029f35eabc5c2ffc
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-07 13:14:17 +0200

    Split webpack for development and production

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,12 @@
 const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { SourceMapDevToolPlugin } = require( 'webpack' );
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
     entry: {
-    //    ToDo: add entries
+        "js/blockForm": "./js/blockForm.js"
     },
-    context: path.resolve(__dirname, 'assets'),
+    context: path.resolve(__dirname, 'assets/'),
     module: {
         rules: [
             {
@@ -29,34 +26,22 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: __dirname + '/dist/',
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new UglifyJsPlugin({
-                include: /\.min\.js$/,
-                sourceMap: true,
-            }),
-        ],
     },
     plugins: [
         new CleanWebpackPlugin({
             verbose: true,
             protectWebpackAssets: false,
-            cleanOnceBeforeBuildPatterns: [],
             cleanAfterEveryBuildPatterns: [
                 '**/*.asset.json',
                 '**/*.asset.php',
             ],
         }),
-        new SourceMapDevToolPlugin({
-            filename: '[file].map',
+        new DependencyExtractionWebpackPlugin({
+            injectPolyfill: true,
         }),
-        new DependencyExtractionWebpackPlugin(),
     ],
     watchOptions: {
         poll: true,
         ignored: /node_modules/,
-    },
+    }
 };
