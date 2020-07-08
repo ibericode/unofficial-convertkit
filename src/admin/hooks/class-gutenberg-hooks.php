2e2c5897f762a1f72ea92c952fbfba5c004fe2ba
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-08 17:21:25 +0200

    Load js from manifest for development purpose only

--- src/admin/hooks/class-gutenberg-hooks.php
+++ src/admin/hooks/class-gutenberg-hooks.php
@@ -12,7 +12,7 @@ class Gutenberg_Hooks implements Hooks {
 	 * @inheritDoc
 	 */
 	public function hook( Hooker $hooker ) {
-		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_scripts' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_assets' ) );
 	}
 
 	/**
@@ -20,7 +20,69 @@ class Gutenberg_Hooks implements Hooks {
 	 *
 	 * @internal
 	 */
-	public function enqueue_scripts() {
+	public function enqueue_assets() {
+		//Development scripts
+		if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && $this->enqueue_dev_assets() ) {
+			return;
+		}
+
 		enqueue_script( 'js/block-form.js' );
+	}
+
+	/**
+	 * @return bool
+	 */
+	private function enqueue_dev_assets(): bool {
+		$manifest = UNOFFICIAL_CONVERKIT_ASSETS_DIR . '/asset-manifest.json';
+
+		if ( ! file_exists( $manifest ) ) {
+			return false;
+		}
+
+		$assets = (array) json_decode( file_get_contents( $manifest ), true );
+
+		if ( empty( $assets ) ) {
+			return false;
+		}
+
+		foreach ( $assets as $asset => $asset_uri ) {
+			$is_js = preg_match( '/\.js$/', $asset_uri );
+			//ToDo: load css if we has any.
+			//          $is_css   = preg_match( '/\.css$/', $asset_uri );
+			//          $is_chunk = preg_match( '/\.chunk\./', $asset_uri );
+
+			if ( ! $is_js ) {
+				continue;
+			}
+
+			if ( $is_js ) {
+				$asset_json = array_search( str_replace( '.js', '.json', $asset ), $assets, true );
+
+				$this->enqueue_script( $asset_uri, $assets[ $asset_json ] ?? '' );
+
+				continue;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param string $asset_uri
+	 * @param string $asset_json
+	 */
+	private function enqueue_script( string $asset_uri, string $asset_json = null ) {
+		$options = array();
+
+		if ( ! empty( $asset_json ) ) {
+			$contents = file_get_contents( $asset_json );
+
+			if ( is_string( $contents ) ) {
+				//The json contains all the dependencies and the version
+				$options = (array) json_decode( $contents, true );
+			}
+		}
+
+		enqueue_script( $asset_uri, $options );
 	}
 }
