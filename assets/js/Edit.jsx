f953674decc0560eceb58b9854e82890734f37fa
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-09 15:40:19 +0200

    Add Inscpector Controls

--- assets/js/Edit.jsx
+++ assets/js/Edit.jsx
@@ -1,10 +1,24 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ConvertKitForm from 'convertkit-react';
+import { InspectorControls } from '@wordpress/editor';
+import { CheckboxControl } from '@wordpress/components';
 
 const Edit = () => {
 	//Todo: make the parameter configurable
-	return <ConvertKitForm formId="1441335" template="charlotte" />;
+	return (
+		<>
+			<InspectorControls>
+				<CheckboxControl
+					heading="Checkbox Field"
+					label="Tick Me"
+					help="Additional help text"
+				/>
+			</InspectorControls>
+			<ConvertKitForm formId={1441335} template="charlotte" />
+			<div>Helklo world</div>
+		</>
+	);
 };
 
 export default hot(Edit);
