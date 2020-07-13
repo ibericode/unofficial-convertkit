545601b17904d8cb125ba2aae359243a4bb6aaff
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-13 16:12:29 +0200

    Add routes to get the forms through the api

---
+++ src/api/controllers/class-forms-controller.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace UnofficialConvertKit\Forms;
+
+use stdClass;
+use function UnofficialConvertKit\get_rest_api;
+
+class Forms_Controller {
+
+	/**
+	 * @return bool
+	 */
+	public function authenticate(): bool {
+		return current_user_can( 'edit_posts' );
+	}
+
+	/**
+	 * @return stdClass
+	 */
+	public function index(): stdClass {
+		//ToDo: cache the request
+		return get_rest_api()->lists_forms();
+	}
+}
---
+++ src/api/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use UnofficialConvertKit\Forms\Forms_Controller;
+
+/**
+ * @see register_rest_route()
+ *
+ * @internal
+ */
+return array(
+	'/forms' => static function() {
+
+		require __DIR__ . '/controllers/class-forms-controller.php';
+		$controller = new Forms_Controller();
+
+		return array(
+			'methods'             => WP_REST_Server::READABLE,
+			'callback'            => array( $controller, 'index' ),
+			'permission_callback' => array( $controller, 'authenticate' ),
+		);
+	},
+);
