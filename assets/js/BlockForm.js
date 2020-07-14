433e388092e75d91c74bbc979d086307a612569f
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-14 16:43:16 +0200

    Remove block icon file

--- assets/js/BlockForm.js
+++ assets/js/BlockForm.js
@@ -2,11 +2,10 @@ import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 import edit from './Edit.jsx';
 import save from './Save.js';
-import icon from './BlockIcon.jsx';
 
 registerBlockType('unofficial-convertkit/form', {
 	title: __('ConvertKit Form'),
-	icon,
+	icon: 'admin-plugins',
 	category: 'embed',
 	attributes: {
 		formUid: {
--- assets/js/BlockIcon.jsx
+++
@@ -1,7 +0,0 @@
-import { hot } from 'react-hot-loader/root';
-import React from 'react';
-import { Icon } from '@wordpress/components';
-
-const BlockIcon = () => <Icon icon="admin-plugins" />;
-
-export default hot(BlockIcon);
