ec92f47d780d619219f8da164878309c46df07db
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-24 11:36:16 +0200

    Add link to settings page in the plugin page

--- src/core/hooks/class-default-hooks.php
+++ src/core/hooks/class-default-hooks.php
@@ -13,6 +13,11 @@ class Default_Hooks implements Hooks {
 		if ( ! is_ajax() ) {
 			add_action( 'init', array( $this, 'register_scripts_from_assets' ) );
 		}
+
+		global $pagenow;
+		if ( 'plugins.php' === $pagenow ) {
+			add_filter( 'plugin_action_links_' . plugin_basename( UNOFFICIAL_CONVERTKIT_PLUGIN_FILE ), array( $this, 'plugin_settings_link' ) );
+		}
 	}
 
 	/**
@@ -25,6 +30,9 @@ class Default_Hooks implements Hooks {
 	/**
 	 * @see UNOFFICIAL_CONVERKIT_ASSETS_DIR
 	 * @see assets.php
+	 *
+	 * @internal
+	 * @ignore
 	 */
 	public function register_scripts_from_assets() {
 		$assets = require UNOFFICIAL_CONVERKIT_ASSETS_DIR . '/assets.php';
@@ -34,5 +42,20 @@ class Default_Hooks implements Hooks {
 			$src                = plugins_url( sprintf( '%s/%s', $relative_asset_dir, $asset ), UNOFFICIAL_CONVERTKIT_PLUGIN_FILE );
 			wp_register_script( $asset, $src, $options['dependencies'] ?? array(), $options['version'] ?? false );
 		}
+	}
+
+	/**
+	 * Add link to settings page.
+	 *
+	 * @param array $links
+	 *
+	 * @return array
+	 * @internal
+	 * @ignore
+	 */
+	public function plugin_settings_link( array $links ): array {
+		$settings_link = sprintf( '<a href="%s">%s</a>', admin_url( 'options-general.php?page=unofficial-convertkit-settings' ), esc_html__( 'Settings', 'unofficial-convertkit' ) );
+		array_unshift( $links, $settings_link );
+		return $links;
 	}
 }
