052b3271045095f2e26a2f2d776efdffe0a85ace
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-28 15:16:28 +0200

    Remove todo

--- src/integrations/hooks/class-registration-form-hooks.php
+++ src/integrations/hooks/class-registration-form-hooks.php
@@ -17,8 +17,5 @@ class Registration_Form_Hooks extends Default_Integration_Hooks {
 		add_action( 'um_after_register_fields', $callable, 20, 0 );
 		add_action( 'register_form', $callable, 20, 0 );
 		add_action( 'woocommerce_register_form', $callable, 20, 0 );
-
-		//TODO: Maybe at this one?
-		//add_action( 'user_new_form', $callable, 20 );
 	}
 }
