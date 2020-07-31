4ac81511b088fc7997835ef9d2c0971a510e25f2
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-31 11:49:05 +0200

    Update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -56,7 +56,7 @@ module.exports = {
 		new MiniCssExtractPlugin({
 			filename: '[name].css',
 		}),
-		new FixStyleOnlyEntriesPlugin({}),
+		new FixStyleOnlyEntriesPlugin(),
 		new CleanWebpackPlugin({
 			verbose: true,
 		}),
