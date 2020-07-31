09a4ff854777529244044490e58b6308945cb56d
Luuk van Kooten <luukvankooten@gmail.com>
2020-07-31 11:45:36 +0200

    Add name off the integration in translate string

--- src/views/integrations/admin/components/table/enable-row.php
+++ src/views/integrations/admin/components/table/enable-row.php
@@ -13,14 +13,18 @@ return static function( Integration $integration ) {
 	$yes_no = require __DIR__ . '/../form/radio-yes-no.php';
 	?>
 	<th>
-		<?php esc_html_e( 'Enabled', 'unofficial-convertkit' ); ?>
+		<?php esc_html_e( 'Activate', 'unofficial-convertkit' ); ?>
 	</th>
 	<td class="nowrap integration-toggles-wrap">
 		<?php
 		$yes_no(
 			sprintf( 'unofficial_convertkit_integrations[%s][enabled]', $integration->get_identifier() ),
 			$integration->is_active(),
-			__( 'Enable the Comment Form integration? This will add a sign-up checkbox to the form.', 'unofficial-convertkit' )
+			sprintf(
+				/* translators: %s the name of the integration */
+				__( 'Activate the %s integration? This will add a sign-up checkbox to the form.', 'unofficial-convertkit' ),
+				$integration->get_name()
+			)
 		);
 		?>
 	</td>
