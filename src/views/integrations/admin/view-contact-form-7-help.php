5b229db880b7c70f4990427e25454762db06d87b
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2020-08-03 12:00:39 +0200

    cs

--- src/views/integrations/admin/view-contact-form-7-help.php
+++ src/views/integrations/admin/view-contact-form-7-help.php
@@ -8,7 +8,7 @@ use UnofficialConvertKit\Integrations\Contact_Form_7_Integration;
  * @see UnofficialConvertkit\Integrations\Admin\Contact_Form_7_Hooks
  */
 return static function() {
-	$input = sprintf( '<input type="text" size="%d" onfocus="this.select()" readonly="" value="[%s]">', strlen(Contact_Form_7_Integration::WPCF7_TAG ) * 0.8, Contact_Form_7_Integration::WPCF7_TAG )
+	$input = sprintf( '<input type="text" size="%d" onfocus="this.select()" readonly="" value="[%s]">', strlen( Contact_Form_7_Integration::WPCF7_TAG ) * 0.8, Contact_Form_7_Integration::WPCF7_TAG )
 	?>
 	<p>
 	<?php
