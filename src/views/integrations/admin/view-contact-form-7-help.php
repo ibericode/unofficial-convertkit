a9e685f9cc1759de9e955cf13be779b394e4ebdb
Luuk van Kooten <luukvankooten@gmail.com>
2020-08-11 14:01:12 +0200

    Fix shortcode documentation

--- src/views/integrations/admin/view-contact-form-7-help.php
+++ src/views/integrations/admin/view-contact-form-7-help.php
@@ -8,7 +8,8 @@ use UnofficialConvertKit\Integrations\Contact_Form_7_Integration;
  * @see UnofficialConvertKit\Integrations\Admin\Contact_Form_7_Hooks
  */
 return static function() {
-	$input = sprintf( '<input type="text" size="%d" onfocus="this.select()" readonly="" value="[%s]">', strlen( Contact_Form_7_Integration::WPCF7_TAG ) * 0.8, Contact_Form_7_Integration::WPCF7_TAG )
+	$shortcode = sprintf( '%s email-field:your-email', Contact_Form_7_Integration::WPCF7_TAG );
+	$input     = sprintf( '<input type="text" size="%d" onfocus="this.select()" readonly="" value="[%s]">', strlen( $shortcode ) * 0.8, $shortcode )
 	?>
 	<p>
 	<?php
