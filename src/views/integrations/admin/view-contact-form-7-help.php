532fd54c8590f39dab3889e9aad6296f5f3abd60
Luuk van Kooten <luukvankooten@gmail.com>
2020-08-13 10:17:36 +0200

    Code style

--- src/views/integrations/admin/view-contact-form-7-help.php
+++ src/views/integrations/admin/view-contact-form-7-help.php
@@ -3,23 +3,21 @@
 use UnofficialConvertKit\Integrations\Contact_Form_7_Integration;
 
 /**
- *
  * @see UnofficialConvertKit\Integrations\Admin\Contact_Form_7_Hooks
  * @internal
- *
  */
 return static function() {
 	$shortcode = sprintf( '%s email-field:your-email', Contact_Form_7_Integration::WPCF7_TAG );
 	$input     = sprintf( '<input type="text" size="%d" onfocus="this.select()" readonly="" value="[%s]">', round( strlen( $shortcode ) / 10 ) * 10, $shortcode )
 	?>
 	<p>
-	<?php
-		printf(
-			/* translators: %s: input form tag  */
-			esc_html__( 'To integrate with Contact Form 7, configure the settings below and then add %s to your CF7 form mark-up.', 'unofficial-convertkit' ),
-			$input
-		);
-	?>
-		</p>
+		<?php
+			printf(
+				/* translators: %s: input form tag  */
+				esc_html__( 'To integrate with Contact Form 7, configure the settings below and then add %s to your CF7 form mark-up.', 'unofficial-convertkit' ),
+				$input
+			);
+		?>
+	</p>
 	<?php
 };
