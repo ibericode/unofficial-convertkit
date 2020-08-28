0b76f19426749e8c139b49a8937d9e82cc9ce0c0
Luuk van Kooten <luukvankooten@gmail.com>
2020-08-28 12:52:46 +0200

    Code style

--- assets/js/Admin.js
+++ assets/js/Admin.js
@@ -3,9 +3,7 @@ import { __ } from '@wordpress/i18n';
 window.addEventListener('load', async () => {
 	const indicator = document.getElementById('uck-indicator');
 	const url = window.wp.ajax.settings.url;
-	const response = await fetch(
-		`${url}?action=unofficial_convertkit_info`
-	);
+	const response = await fetch(`${url}?action=unofficial_convertkit_info`);
 	const { status, account } = await response.json();
 
 	const state = {
@@ -29,12 +27,16 @@ window.addEventListener('load', async () => {
 	indicator.textContent = state.text;
 
 	if (false === account) {
-		document.getElementById('uck-account-status-text').innerText = __('No information about your account.', 'unofficial-convertkit');
+		document.getElementById('uck-account-status-text').innerText = __(
+			'No information about your account.',
+			'unofficial-convertkit'
+		);
 		return;
 	}
 
 	document.getElementById('uck-account-status').style.display = 'none';
 	document.getElementById('uck-account-info').style.removeProperty('display');
 	document.getElementById('uck-account-name').innerText = account.name ?? '';
-	document.getElementById('uck-account-email').innerText = account.primary_email_address ?? '';
+	document.getElementById('uck-account-email').innerText =
+		account.primary_email_address ?? '';
 });
